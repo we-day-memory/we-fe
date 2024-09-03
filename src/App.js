bf15e0085d7b7dbcb4bfaa29d9e3e0b636cb2f3e
nikola.vojnovic1 <nikola.vojnovic@klikdousluge.com>
2024-09-03 21:54:26 +0200

    Update code to be more readable
    Signed-off-by: nikola.vojnovic1 <nikola.vojnovic@klikdousluge.com>

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import PhotoUploader from "./components/PhotoUploader";
 import Notification from "./components/notif";
 import "./assets/main.scss";
-import texts from './constants/texts.js';
+import texts from "./constants/texts.js";
 
 function App() {
   const [notification, setNotification] = useState(null);
@@ -15,14 +15,24 @@ function App() {
   };
 
   const handleUploadError = (error) => {
-    setNotification({ message: `${texts.errors.general} ${error}`, type: "error" });
+    setNotification({
+      message: `${texts.errors.general} ${error}`,
+      type: "error",
+    });
   };
 
   return (
     <div className="App">
-      <div className="welcome-text">
-        <p>{texts.header.introMessage}</p>
+      <div className="header-image-border">
+        <div className="header-image"></div>
+      </div>
+
+      <div className="header">
         <h1>{texts.header.brideName} i {texts.header.groomName}</h1>
+      </div>
+
+      <div className="date">
+      <p>{texts.header.date}</p>
       </div>
 
       <PhotoUploader
